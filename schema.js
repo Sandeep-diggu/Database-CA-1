47c8216aa8f3ddc7261886bd66e686c2c985495b
Sandeep-diggu <sandeepkumar684@gmail.com>
2025-04-08 14:55:41 +0530

    done

---
+++ schema.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const {Restarent schema } = mongoose
+const { Restarent schema: schemaTypes } = mongoose.Schema
+
+
+const {name}:
+            type:string,
+            required=true,
+
+const {city type}:
+            type:string,
+            require=true,
+
+const {dish type}:
+            type:string,
+            require=true,
+
+const {cost type}:
+            type:Number,
+            require=true,
+
+        
+
+         
